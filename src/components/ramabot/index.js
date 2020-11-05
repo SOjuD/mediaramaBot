4135f6c0e11ec06fbdfbc61a24ab34d5cf40afb4
Oleg <talrash666@gmail.com>
2020-11-05 14:51:09 +0300

    rewrite access to data

--- src/components/ramabot/index.js
+++ src/components/ramabot/index.js
@@ -1,8 +1,9 @@
 import { createChat } from './chat-control';
 import { getBotCookie, getParams, addMask } from './functions';
 
-
-const pathToParams = 'params.json';
+const id = window.location.search;
+const basePath = 'http://ramabot.dix.by/getparams.php';
+const pathToParams = basePath + id;
 
 getParams(pathToParams).then( ramaParams => {
     ramaParams.log = getBotCookie().log || [];
