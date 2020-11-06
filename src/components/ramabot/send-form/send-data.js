08823c5409f95773fce56d65ec685953f64fee09
An1984ton <An1984tonn@ya.ru>
2020-11-06 16:20:50 +0300

    add something

--- src/components/ramabot/send-form/send-data.js
+++ src/components/ramabot/send-form/send-data.js
@@ -1,10 +1,16 @@
 export default function sendData(params) {
+    let dataStr = '';
+
+    params.log.forEach( ({ msg }) => {
+        dataStr += `${msg}\n`;
+    } );
+
     fetch(params.handlerUrl, {
         method: 'POST',
         header: {
             // 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'text/plain;charset=utf-8'
         },
-        body: JSON.stringify(params.log)
+        body: dataStr
     })
 }
